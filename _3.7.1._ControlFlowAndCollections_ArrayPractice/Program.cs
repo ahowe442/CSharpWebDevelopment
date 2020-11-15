251ac3aab081f075713564283ce88564f9bfb110
Abby Howe <howe.abigail@gmail.com>
2020-11-15 16:28:18 -0600

    Added All Chapter 3 Notes, Exercises and  Studio.

---
+++ _3.7.1._ControlFlowAndCollections_ArrayPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._7._1._ControlFlowAndCollections_ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TODO: Create and initialize an array with the following values in a single line:
+
+            1, 1, 2, 3, 5, 8
+
+            //Loop through the array and print out each value.
+
+            //Modify the loop to only print the odd numbers from the array.*/
+        }
+    }
+}
---
+++ _3._7._4._ControlFlowAndCollections_BonusMission/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3._7._4._ControlFlowAndCollections_BonusMission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TODO: Update your solution to the List Practice section using to 
+             * use the string from the Array Practice section. Search “C# 
+             * convert string to list” online to see how to split a string 
+             * into the more flexible List collection. */
+        }
+    }
+}
