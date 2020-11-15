b01a3a93b7c757c7f186a9d1d374452bceb88b3e
Abby Howe <howe.abigail@gmail.com>
2020-11-15 17:10:49 -0600

    Updates 3.7.1-3.7.4 files and README

---
+++ _3.7.2._ControlFlowAndCollections_StringPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3._7._2._ControlFlowAndCollections_StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TODO: For this exercise, create a string for the value:
+
+            I would not, could not, in a box. I would not, could not with a fox.
+            I will not eat them in a house. I will not eat them with a mouse.
+            Use the Split method to divide the string at each space and store the individual words in an array. If you need to review the method syntax, look back at the string methods table.
+
+            //Print the array of words to verify that your code works. The syntax is:
+
+            //Console.WriteLine(string.Join(",", arrayName));
+
+            //Repeat steps 2 and 3, but change the delimiter to split the string into separate sentences.
+
+             */
+        }
+    }
+}
---
+++ _3.7.3._ControlFlowAndCollections_ListPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._7._3._ControlFlowAndCollections_ListPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* TODO: Make a program similar to GradebookDictionary that does the following:
+
+                //It takes in student names and ID numbers (as integers) instead of names and grades.
+                //The keys should be the IDs and the values should be the names.
+                //Modify the roster printing code accordingly.
+            */
+
+        }
+    }
+}
