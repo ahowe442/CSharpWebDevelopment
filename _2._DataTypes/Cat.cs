b89d7f5357e78b6dee72e6fc13d22f4c1068fd56
Abby Howe <howe.abigail@gmail.com>
2020-11-15 14:41:15 -0600

    * Cat.cs: * Lecture.cs: * Message.cs: * _2._DataTypes.csproj:
    * .gitignore: Added Chapter 2 Data Types

---
+++ _2._DataTypes/Cat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _2._DataTypes
+{
+    public class Cat
+    {
+        string name;
+
+        public Cat()
+        {
+            name = "cat";
+        }
+
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        //2.4.2 Static Methods
+        public static void makeNoise(String noise)
+        {
+            if (noise == null)
+            {
+                Console.WriteLine("Roar!");
+            }
+            else
+            {
+                Console.WriteLine(noise);
+            }
+        }
+    }
+}
---
+++ _2._DataTypes/Message.cs
@@ -0,0 +1,27 @@
+﻿namespace _2._DataTypes
+{
+    class Message
+    {
+        public static string GetMessage(string lang)
+        {
+            if (lang.Equals("sp"))
+            {
+                return "Hola Mundo";
+            }
+            else if (lang.Equals("fr"))
+            {
+                return "Bonjour le monde";
+            }
+            else
+            {
+                return "Hello World";
+            }
+        }
+
+
+        private static string GetPrivateMessage(string hi)
+        {
+            return "Secret message: " + hi;
+        }
+    }
+}
