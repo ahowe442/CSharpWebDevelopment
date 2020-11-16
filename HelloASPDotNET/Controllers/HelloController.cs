9a22bea34a51695b13bc58557c40cf5f89aeb67e
Abby H <howe.abigail@gmail.com>
2020-11-16 16:03:05 -0600

    adds default and optional query params

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -16,5 +16,13 @@ namespace HelloASPDotNET.Controllers
             string html = "<h1>Hello World</h1>";
             return Content(html, "text/html");
         }
+
+        //GET /hello/welcome
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
     }
 }
