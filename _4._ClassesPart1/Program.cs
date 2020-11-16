73771fb3c093e9760c3cedcba0c2239dcb4e5cdd
Abby Howe <howe.abigail@gmail.com>
2020-11-15 19:12:18 -0600

    Add Chapt 4 lecture notes

---
+++ _4._ClassesPart1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._ClassesPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
