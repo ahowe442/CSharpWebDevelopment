9f2c94414533605570218dde10d3a8c5c138dc53
Abby Howe <howe.abigail@gmail.com>
2020-11-15 20:15:43 -0600

    4,5,& 6

--- _4.8._Classes_ResturauntMenu/Program.cs
+++ _4.8._Classes_ResturauntMenu/Program.cs
@@ -2,11 +2,20 @@
 
 namespace _4._8._Classes_ResturauntMenu
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("4.8 Studio: Resturaunt Menu");
+
+            Menu frenchFries = new Menu(5.38, "French Fries", "Potatos that " +
+                "have been sliced into long thin strips and deep fried.  " +
+                "Great with ketchup.", "SIDE", true);
+
+
         }
+
+
     }
 }
--- _5.6._Studio_ResturauntMenuPt2/Program.cs
+++ _5.6._Studio_ResturauntMenuPt2/Program.cs
@@ -2,11 +2,82 @@
 
 namespace _5._6._Studio_ResturauntMenuPt2
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creates instance of MenuItems.
+            //MenuItem coke = new MenuItem("Coke", 1.99, "Bubbly cola drink with ice in a glass", "Drinks", "October");
+            //MenuItem sirloin10 = new MenuItem("10oz. Sirloin", 14.99, "never frozen","Main Course", "September");
+            //MenuItem iceCream = new MenuItem("Vanilla Ice Cream", 3.99, "cold milk treat", "Dessert", "October");
+            //Menu.printItem(coke);
+            //Menu.printItem(sirloin10);
+            //Menu.printItem(iceCream);
         }
+    }
+    public class MenuItem
+    {
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string MonthAdded { get; set; }
+
+        public MenuItem(string itemName, double price, string description, string category, string monthAdded)
+        {
+            ItemName = itemName;
+            Price = price;
+            Description = description;
+            Category = category;
+            MonthAdded = monthAdded;
+        }
+
+
+    }
+
+    public class Menu
+    {
+        List<MenuItem> menuItems = new List<MenuItem>();
+
+        //add item to menu
+        addToMenu()
+        {
+
+        }
+
+        //remove item from menu
+        removeFromMenu()
+        {
+
+        }
+
+        //tell if a menu item is new.
+        isNew()
+        {
+
+        }
+
+        //tell when the menu was last updated.
+        dateLastUpdated()
+        {
+
+        }
+
+        // print out a single menu item.
+        public static void printItem(MenuItem item)
+        {
+            Console.WriteLine(item.ItemName + "is " + item.Description + "costs: " + item.Price + "category: " + item.Category + "Date added: " + item.MonthAdded);
+        }
+
+
+        //print out the entire menu.
+        printMenu()
+        {
+
+        }
+
+        //A way to determine whether or not two menu items are equal.
     }
 }
