2ecdef024d562f686dc4d58decf41deac4219acb
Abby Howe <howe.abigail@gmail.com>
2020-11-15 19:46:17 -0600

    Adds 4.8 and 5.6 Resturaunt Menu parts 1 & 2

---
+++ _5.6._Studio_ResturauntMenuPt2/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿namespace _5._6._Studio_ResturauntMenuPt2
+{
+    public class MenuItem
+    {
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string MonthAdded { get; set; }
+
+        public MenuItem(string itemName, double price, string description, string category, string monthAdded)
+        {
+            ItemName = itemName;
+            Price = price;
+            Description = description;
+            Category = category;
+            MonthAdded = monthAdded;
+        }
+
+        public string getMenuItem()
+        {
+            return this.ItemName + "is " + this.Description + "costs: " + this.Price + "category: " + Category + "Date added: " + DateAdded;
+        }
+    }
+}
---
+++ _5.6._Studio_ResturauntMenuPt2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5._6._Studio_ResturauntMenuPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
