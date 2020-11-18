761d0c15def66a25c53789b2cc0c7eacdb6195bd
Abby Howe <howe.abigail@gmail.com>
2020-11-17 18:37:39 -0600

    7.8_Inheritance Cats update

--- _7.8._Inheritance_Exercises_Cats/CatTests/CatTests.cs
+++ _7.8._Inheritance_Exercises_Cats/CatTests/CatTests.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _7._8._Inheritance_Exercises_Cats;
+
+//TODO: Learn how to configure this without thet test template
+
 namespace _7._8._Inheritance_Exercises_Cats.CatTests
 {
+    [TestClass]
     public class CatTests
     {
-        public CatTests()
+        [TestMethod]
+        public void InheritsBaseInFirstConstructor()
         {
+            HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+            Assert.AreEqual(7, keyboardCat.Weight, .001);
         }
     }
 }
--- _7.8._Inheritance_Exercises_Cats/Program.cs
+++ _7.8._Inheritance_Exercises_Cats/Program.cs
@@ -2,11 +2,32 @@
 
 namespace _7._8._Inheritance_Exercises_Cats
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
+
+            // Try out your skills with inheritance here!
+
+            var garfield = new HouseCat("Garfield", 16.5);
+            garfield.Eat();
+            Console.WriteLine(garfield.Tired);
+
+            HouseCat spike = new HouseCat("Spike");
+            Console.WriteLine(spike.Weight);
+
+            Cat plainCat = new Cat(8.6);
+            HouseCat cheshireCat = new HouseCat("Cheshire Cat", 26);
+            Console.WriteLine(plainCat.Noise());
+            Console.WriteLine(cheshireCat.Noise());
+
+            Cat suki = new HouseCat("Suki", 8.0);
+            Console.WriteLine(suki.Noise());
+            Console.WriteLine((suki as HouseCat).IsSatisfied());
+
         }
     }
 }
