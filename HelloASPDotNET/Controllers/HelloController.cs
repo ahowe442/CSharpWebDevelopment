5886839a49ef74f9c37d144c42b6be5878061bdf
Abby Howe <howe.abigail@gmail.com>
2020-11-18 16:26:08 -0600

    refactor HelloController Index() - Get request and Welcome()-Post request

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -11,15 +11,19 @@ namespace HelloASPDotNET.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            string html = "<h1>Hello World</h1>";
+            string html = "<form method='post' action='/hello'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
             return Content(html, "text/html");
         }
 
-        //GET /hello/welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")]
+        [HttpPost]
+        [Route("/hello")]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
