8d7bfbb62e19432056d1aa4d587cf39da41e61b3
Abby Howe <howe.abigail@gmail.com>
2020-11-17 22:06:00 -0600

    Creates the model; adds properties to the User class

--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -3,8 +3,13 @@ namespace SpaDay.Models
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } //Prob don't want to set?? Get only??
+
         public User()
         {
         }
+
     }
 }
